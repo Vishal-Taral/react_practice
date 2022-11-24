5a73313215ae341c8e27623ecc4dc2cfa3097485
vishal <vishaltaral09@gamil.com>
2022-11-24 16:40:38 +0530

    First Component Created in react app without css file

--- React_App/first-react-app/src/App.js
+++ React_App/first-react-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello.. World..!  I am Vishal</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ React_App/first-react-app/src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <h5>First Component of in react App i.e Expense Component</h5>
+        <p>ExpenseItem Component Created</p>
+    </div>
+);
+}
+
+export default ExpenseItem;
