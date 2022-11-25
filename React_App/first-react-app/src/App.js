7f4ce7bc07b499ad04d41c829ee15c754e178765
vishal <vishaltaral09@gamil.com>
2022-11-25 12:33:37 +0530

    data accessed form parent component in child components

--- React_App/first-react-app/src/App.js
+++ React_App/first-react-app/src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
 
 function App() {
+  let empInfo = [
+    {
+      id: 1,
+      name: "vishal",
+      joiningDate: new Date(2022, 9, 26),
+    },
+    {
+      id: 2,
+      name: "rohan",
+      joiningDate: new Date("05 October 2011 14:48 UTC"),
+    },
+    {
+      id: 3,
+      name: "ataharva",
+      joiningDate: new Date(2022, 1, 26),
+    },
+
+  ];
   return (
     <div className="App">
-      <h1>Hello.. World..!  I am Vishal</h1>
+      <h1>Hello.. World..! I am Vishal</h1>
       <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem empName={empInfo[0].name} empId = {empInfo[0].id} empJoining = {empInfo[0].joiningDate}/>
+      <ExpenseItem empName={empInfo[1].name} empId = {empInfo[1].id} empJoining = {empInfo[1].joiningDate}/>
+      <ExpenseItem empName={empInfo[2].name} empId = {empInfo[2].id} empJoining = {empInfo[2].joiningDate}/>
+      {/**We can reuse the component easily with less coding*/}
     </div>
   );
 }
--- React_App/first-react-app/src/components/ExpenseItem.js
+++ React_App/first-react-app/src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const birthDate = new Date (1999,12,14);
-    const birthdayBoy = 'Vishal';
-    let gift = 'Car';
-    return (
+function ExpenseItem (props) {
+//    console.log(props.empName,props.empjoining); 
+        const birthDate = new Date (1999,12,14);
+        const birthdayBoy = 'Vishal';
+        let gift = 'Car';
+   
+
+        return (
     <div className='mainDiv'>
-    
         <h5>{Math.random()}</h5> {/* we can write expression also in curly bracks */ }
         <h4>{birthDate.toISOString()} </h4>
         <h4>{birthdayBoy} </h4>
         <h4>{gift}</h4>
+        <h1>{props.empName}</h1>
+        <h1>{props.empId}</h1>
+        <p>{props.empJoining?.toISOString()}</p>
         <p>ExpenseItem Component Created</p>
     </div>
 );
