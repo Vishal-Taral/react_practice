634f265ae0d08053bdc2a9dcf0e888976aabd532
apurv <apurv.khadamkar@radixhealth.com>
2022-12-04 22:51:13 +0530

    new project created for practice

---
+++ React_App/4-12-new-practice/src/App.js
@@ -0,0 +1,13 @@
+import Expenses from './components/expeses/expenses';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Expenses/>
+      <h1> Hey.. Hi This is Vishal</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ React_App/4-12-new-practice/src/components/expeses/expenses.js
@@ -0,0 +1,11 @@
+import './expenses.css';
+
+const Expenses = () => {
+    return ( 
+        <div className="expenses">
+            <h1>Expenses Component</h1>
+        </div>
+    );
+}
+
+export default Expenses;
