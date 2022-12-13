414b828d930fa697e2159e4bbdbc603c83c5fb35
vishal <vishaltaral09@gmail.com>
2022-12-13 23:30:23 +0530

    todo list is complted with remove all button

--- React_App/todo-list/src/App.js
+++ React_App/todo-list/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import TodoList from './Components/TodoList/TodoList';
+import "./App.css";
+import TodoList from "./Components/TodoList/TodoList";
 
 function App() {
   return (
     <>
       <div className="App">
-          <TodoList />
+        <TodoList />
       </div>
     </>
   );
--- React_App/todo-list/src/Components/List/List.js
+++ React_App/todo-list/src/Components/List/List.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import './List.css'
+import React from "react";
+import "./List.css";
 const List = (props) => {
-    const onRemoveActivity = () => {
-        props.toRemove();
-    }
+  const onRemoveActivity = () => {
+    props.toRemove();
+  };
+
+  const removeAllActivity = () => {
+    props.toRemoveAll();
+  }
   return (
     <div>
-        <strong> Here is Activity List</strong>
-        <ul>{props.data.map((activities , index) => {
-            return (
-                <div className='list-item' key={index}>
-                    <li className='act-list'>{activities}</li>
-                    <button className='remove-btn' onClick={onRemoveActivity}>Remove</button>
-                </div>
-                
-            )
-        })}</ul>
-        
+      <strong> Here is Activity List</strong>
+      <ul>
+        {props.data.map((activities, index) => {
+          return (
+            <div className="list-item" key={index}>
+              <li className="act-list">{activities}</li>
+              <button className="remove-btn" onClick={onRemoveActivity}>
+                Remove
+              </button>
+            </div>
+          );
+        })}
+        {props.data.length >= 1 && <button className=" remove-all" onClick={removeAllActivity}>Remove All</button>}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
